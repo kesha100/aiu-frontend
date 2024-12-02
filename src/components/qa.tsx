fad60f27228e3bfd46c862561388a387b0a7166e
Demiurg20 <islamasylbekov1012@gmail.com>
2024-12-02 17:31:27 +0600

    removing MessageCircle

--- src/components/qa.tsx
+++ src/components/qa.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { Users, MessageCircle, PlusCircle, Send } from 'lucide-react'
+import { Users, PlusCircle, Send } from 'lucide-react'
 
 type Answer = {
   id: number
